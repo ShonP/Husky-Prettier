9111548afb0ffe85210e5b9a93a525f0bd3be2a9
ShonP <pazshon1@gmail.com>
2021-04-19 20:15:31 +0300

    check

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ const App: FunctionComponent = () => {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p> 
+        <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
